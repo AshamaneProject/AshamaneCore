59b04b66e17dcd678dcb61aff1f46e350050500c
ariel- <ariel-@users.noreply.github.com>
2017-01-23 18:33:34 -0300

    Core/Spell: unified handling of SPELL_ATTR5_USABLE_WHILE_* attributes
    Allowed mechanic mask is calculated on startup and auras checked against those mechanics
    Closes #18798
    
    (cherrypicked from 3544577e48f3cf4fe4eae670facb3e5f77608e68)
    (cherrypicked from 53eb6ec61628147046f1e8996e05921a00726c11)

--- src/server/game/Spells/SpellInfo.h
+++ src/server/game/Spells/SpellInfo.h
@@ -684,6 +684,8 @@ class TC_GAME_API SpellInfo
         bool CanSpellProvideImmunityAgainstAura(SpellInfo const* auraSpellInfo) const;
         bool SpellCancelsAuraEffect(AuraEffect const* aurEff) const;
 
+        uint32 GetAllowedMechanicMask() const;
+
     private:
         // loading helpers
         void _InitializeExplicitTargetMask();
@@ -707,6 +709,7 @@ class TC_GAME_API SpellInfo
         AuraStateType _auraState;
 
         SpellDiminishInfo _diminishInfo;
+        uint32 _allowedMechanicMask;
 };
 
 #endif // _SPELLINFO_H
