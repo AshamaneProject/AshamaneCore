aa832aceabfe5ca0ca6a86fcf0a8fc49e5a84ae2
Ulduar <Ulduar@users.noreply.github.com>
2018-07-01 15:50:19 +0500

    Update 2018_07_01_01_world_scriptname.sql

--- sql/ashamane/world/2018_07_01_01_world_scriptname.sql
+++ sql/ashamane/world/2018_07_01_01_world_scriptname.sql
@@ -2,6 +2,8 @@ DELETE FROM `areatrigger_template_actions` WHERE `AreaTriggerId` = 12880;
 INSERT INTO `areatrigger_template_actions` (`AreaTriggerId`, `ActionType`, `ActionParam`, `TargetType`) VALUES
 ('12880','0','227848','2');
 
+UPDATE `spell_areatrigger` SET `DecalPropertiesId`='86' WHERE `SpellMiscId`='8812';
+
 DELETE FROM spell_script_names WHERE scriptname = "spell_gen_maiden_of_virtue_mass_repentance";
 DELETE FROM spell_script_names WHERE scriptname = "spell_gen_maiden_of_virtue_holy_bulwark";
 DELETE FROM spell_script_names WHERE scriptname = "spell_gen_maiden_of_virtue_holy_wrath";
