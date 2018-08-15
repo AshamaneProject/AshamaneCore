cb43906779419d8cdc306e9b874fe5b759a01820
leviathonbeast <leviathonbeast@googlemail.com>
2018-08-15 15:01:31 +0200

    World/DB fix the force cancel black screen i hope.
    changed official flag from 9 to 2 to stop crashes

--- sql/ashamane/world/2018_08_12_01_scene_template.sql
+++ sql/ashamane/world/2018_08_12_01_scene_template.sql
@@ -1,21 +1,22 @@
 -- Intro Cinematic Scene Templates
+-- changed official flag from 9 to 2 to stop crashes
 
 -- Nightborne
 DELETE FROM `scene_template` WHERE (`SceneId`=1900 AND `ScriptPackageID`=2007);
 INSERT INTO `scene_template` (`SceneId`, `Flags`, `ScriptPackageID`) VALUES
-(1900, 9, 2007);
+(1900, 2, 2007);
 
 -- Hightmountain Tauren
 DELETE FROM `scene_template` WHERE (`SceneId`=1901 AND `ScriptPackageID`=1984);
 INSERT INTO `scene_template` (`SceneId`, `Flags`, `ScriptPackageID`) VALUES
-(1901, 9, 1984);
+(1901, 2, 1984);
 
 -- Lightforged Dranai
 DELETE FROM `scene_template` WHERE (`SceneId`=1902 AND `ScriptPackageID`=2005);
 INSERT INTO `scene_template` (`SceneId`, `Flags`, `ScriptPackageID`) VALUES
-(1902, 9, 2005);
+(1902, 2, 2005);
 
 -- Void Elf
 DELETE FROM `scene_template` WHERE (`SceneId`=1903 AND `ScriptPackageID`=2006);
 INSERT INTO `scene_template` (`SceneId`, `Flags`, `ScriptPackageID`) VALUES
-(1903, 9, 2006);
+(1903, 2, 2006); 
