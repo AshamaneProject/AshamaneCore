1834b0a63422b6553cfd15eabba744b3e00b2ded
HannibalRoG <edreisantafe@gmail.com>
2018-09-12 23:44:47 -0400

    Missing condition

--- sql/ashamane/world/2018_09_12_05_Management Material.sql
+++ sql/ashamane/world/2018_09_12_05_Management Material.sql
@@ -76,3 +76,8 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@DEMORALIZED_PEON*100, 9, 3, 0, 0, 0, 100, 0, 0, 0, 0, 0, 66, 0, 0, 0, 0, 0, 0, 12, 1, 0, 0, 0, 0, 0, 0, 'Demoralized Peon - Update - Set Orientation to Invoker'),
 (@DEMORALIZED_PEON*100, 9, 4, 0, 0, 0, 100, 0, 5000, 5000, 0, 0, 45, 1, 1, 0, 0, 0, 0, 19, @LUMBER_BUNNY, 30, 0, 0, 0, 0, 0, 'Demoralized Peon - Update - Despawn Self'),
 (@DEMORALIZED_PEON*100, 9, 5, 0, 0, 0, 100, 0, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Demoralized Peon - Update - Despawn Self');
+
+-- Conditions
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` IN (15) AND `SourceGroup` IN (10338);
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `sourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
+(15, 10338, 0, 0, 0, 9, 0, 13640, 0, 0, 0, 0, 0, '', 'Gossip Only Shows if Quest Taken');
