996d6ce8e9b410b22d5336e9ee8c74b9c5b49012
RoG_Hannibal <edreisantafe@gmail.com>
2018-09-21 05:32:25 -0400

    spells/Hunter Fix Bursting shoot & Sentinel & True aim (#110)
    * Fix Hunter spells
    -Bursting shoot
    -Sentinel
    -True aim
    Closes https://bt.ashamane.com/view.php?id=560 and https://bt.ashamane.com/view.php?id=554
    
    * Update spell_hunter.cpp
    
    * Fixes from Traesh comments
    
    * Update spell_hunter.cpp again

---
+++ sql/ashamane/world/2018_09_08_02_world_areatrigger_template.sql
@@ -0,0 +1,3 @@
+/*Areatrigger*/
+DELETE FROM `areatrigger_template` WHERE (`Id`) IN ('3769');
+INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES ('3769', '4', '0', '10', '10', '7', '7', '3', '3', 'at_hun_sentinel', '0');
