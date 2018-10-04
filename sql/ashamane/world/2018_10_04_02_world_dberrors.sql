e398372399d418e916e15d9d97044d7e0056a9ef
HannibalRoG <edreisantafe@gmail.com>
2018-10-04 04:21:57 -0400

    DB/World fix DB Errors

---
+++ sql/ashamane/world/2018_10_04_02_world_dberrors.sql
@@ -0,0 +1,134 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=158241 AND `ScriptName`='spell_highmaul_blaze_dot';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(158241, 'spell_highmaul_blaze_dot');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=163663 AND `ScriptName`='spell_highmaul_flamethrower_override';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(163663, 'spell_highmaul_flamethrower_override');
+
+DELETE FROM `areatrigger_scripts` WHERE `entry`=10094;
+INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES 
+(10094, 'areatrigger_at_highmaul_infested_waters');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=168374 AND `ScriptName`='areatrigger_highmaul_phemos_blaze';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(168374, 'areatrigger_highmaul_phemos_blaze');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=162901 AND `ScriptName`='areatrigger_highmaul_phemos_blaze';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(162901, 'areatrigger_highmaul_phemos_blaze');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=163042 AND `ScriptName`='areatrigger_highmaul_pale_vitriol';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(163042, 'areatrigger_highmaul_pale_vitriol');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=173824 AND `ScriptName`='areatrigger_highmaul_wild_flames';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(173824, 'areatrigger_highmaul_wild_flames');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=161330 AND `ScriptName`='areatrigger_highmaul_suppression_field';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(161330, 'areatrigger_highmaul_suppression_field');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=172747 AND `ScriptName`='areatrigger_highmaul_expel_magic_frost';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(172747, 'areatrigger_highmaul_expel_magic_frost');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=161574 AND `ScriptName`='areatrigger_highmaul_overflowing_energy';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(161574, 'areatrigger_highmaul_overflowing_energy');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=172915 AND `ScriptName`='areatrigger_highmaul_expel_magic_fel';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(172915, 'areatrigger_highmaul_expel_magic_fel');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=161634 AND `ScriptName`='areatrigger_highmaul_molten_bomb';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(161634, 'areatrigger_highmaul_molten_bomb');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=159202 AND `ScriptName`='areatrigger_highmaul_flame_jet';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(159202, 'areatrigger_highmaul_flame_jet');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=159412 AND `ScriptName`='areatrigger_highmaul_mauling_brew';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(159412, 'areatrigger_highmaul_mauling_brew');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=158639 AND `ScriptName`='areatrigger_highmaul_orb_of_chaos';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(158639, 'areatrigger_highmaul_orb_of_chaos');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=176525 AND `ScriptName`='areatrigger_highmaul_growing_shadows';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(176525, 'areatrigger_highmaul_growing_shadows');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=159489 AND `ScriptName`='areatrigger_highmaul_mind_fungus';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(159489, 'areatrigger_highmaul_mind_fungus');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=173241 AND `ScriptName`='areatrigger_highmaul_spore_shot';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(173241, 'areatrigger_highmaul_spore_shot');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=173229 AND `ScriptName`='areatrigger_highmaul_creeping_moss';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(173229, 'areatrigger_highmaul_creeping_moss');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=160413 AND `ScriptName`='areatrigger_highmaul_call_of_the_tides';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(160413, 'areatrigger_highmaul_call_of_the_tides');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=162371 AND `ScriptName`='areatrigger_highmaul_crystalline_barrage';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(162371, 'areatrigger_highmaul_crystalline_barrage');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=162894 AND `ScriptName`='areatrigger_highmaul_gift_of_earth';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(162894, 'areatrigger_highmaul_gift_of_earth');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=175648 AND `ScriptName`='areatrigger_highmaul_rune_of_disintegration';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(175648, 'areatrigger_highmaul_rune_of_disintegration');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=175047 AND `ScriptName`='areatrigger_highmaul_arcane_residue';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(175047, 'areatrigger_highmaul_arcane_residue');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=70253 AND `ScriptName`='spell_summon_gasbot';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(70253, 'spell_summon_gasbot');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=116235 AND `ScriptName`='at_amethyst_pool';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(116235, 'at_amethyst_pool');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=115817 AND `ScriptName`='at_nullification_barrier';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(115817, 'at_nullification_barrier');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=116546 AND `ScriptName`='at_draw_power';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(116546, 'at_draw_power');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=122731 AND `ScriptName`='at_cancelling_noise';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(122731, 'at_cancelling_noise');
+
+UPDATE `creature_template` SET `ScriptName`='npc_highmaumaggot' WHERE  `entry`=80728;
+UPDATE `creature_template` SET `ScriptName`='npc_lady_sylvanas_windrunner' WHERE  `entry`=10181;
+UPDATE `creature_template` SET `ScriptName`='npc_fourth_and_goal_deathwing' WHERE  `entry`=48572;
+UPDATE `creature_template` SET `ScriptName`='npc_kezan_gasbot' WHERE  `entry`=37598;
+UPDATE `gameobject_template` SET `ScriptName`='go_highmaul_chain' WHERE  `entry`=233127;
+
+DELETE FROM `criteria_data` WHERE `criteria_id`=24693 AND `type`=11;
+INSERT INTO `criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES 
+(24693, 11, 0, 0, 'achievement_highmaul_pair_annihilation');
+
+DELETE FROM `criteria_data` WHERE `criteria_id`=6455 AND `type`=41;
+INSERT INTO `criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES 
+(6455, 41, 0, 0, 'achievement_show_me_you_moves');
+
+-- Foreman Dampwick remove SAI
+SET @ENTRY := 35769;
+UPDATE `creature_template` SET `AIName`="" WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
