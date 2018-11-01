15661da431bc668ec15222d262dff3f891d34803
Osleyder Gonzalez Acosta <osleyder.gonzalez@gmail.com>
2018-11-01 01:23:44 -0400

    Clean DisplayID are from BfA 8.0.1
    These displayID are from Battle by Azeroth 8.0.1. This is an unnecessary error on the server.

---
+++ sql/ashamane/world/2018_11_01_01_cleaning_dberrors.sql
@@ -0,0 +1,2 @@
+-- Table `creature_model_info` has a non-existent DisplayID (ID: 84345). Skipped.
+DELETE FROM `creature_model_info` WHERE `DisplayID` IN (84345, 87473, 87586, 87441, 87446, 87474, 87460, 87448, 87470, 87447, 87439, 87435, 87461, 87450, 87440);
