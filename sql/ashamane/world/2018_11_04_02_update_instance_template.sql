73d3d39e5daeab2be92b024aea835f67d6a6d928
Osleyder Gonzalez Acosta <osleyder.gonzalez@gmail.com>
2018-11-04 18:14:04 -0500

    Some dungeons were updated for 7.3.5
    Some dungeons were updated for 7.3.5

---
+++ sql/ashamane/world/2018_11_04_02_update_instance_template.sql
@@ -0,0 +1,212 @@
+DELETE FROM `instance_template` WHERE `map` IN (169, 734, 930, 951, 971, 980, 998, 999, 1001, 1005, 1007, 1010, 1014, 1024, 1029, 1030, 1031, 1035, 1048, 1049, 1050, 
+1051, 1060, 1061, 1062, 1095, 1099, 1102, 1103, 1104, 1105, 1106, 1107, 1112, 1120, 1121, 1126, 1130, 1131, 1134, 1135, 1144, 1148, 1155, 1157, 1161, 1168, 1170, 1171, 1191,
+1200, 1203, 1207, 1268, 1280, 1329, 1374, 1402, 1431, 1451, 1453, 1455, 1460, 1461, 1464, 1468, 1469, 1476, 1478, 1479, 1480, 1481, 1489, 1494, 1495, 1500, 1502, 1503,
+1504, 1505, 1511, 1512, 1513, 1514, 1515, 1519, 1522, 1523, 1526, 1528, 1529, 1530, 1536, 1537, 1539, 1540, 1541, 1545, 1552, 1553, 1557, 1572, 1579, 1580, 1583, 1599,
+1600, 1602, 1604, 1605, 1607, 1608, 1609, 1611, 1612, 1616, 1617, 1618, 1620, 1621, 1623, 1624, 1625, 1626, 1629, 1630, 1632, 1646, 1648, 1651, 1653, 1662, 1666, 1672,
+1673, 1677, 1681, 1683, 1684, 1687, 1692, 1693, 1695, 1698, 1702, 1703, 1704, 1705, 1706, 1707, 1708, 1710, 1714, 1715, 1719, 1728, 1729, 1730, 1731, 1732, 1734, 1736,
+1737, 1738);
+
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`, `insideResurrection`) VALUES
+(734,  0,    'instance_anquiraj_event',                         1, 0),
+(930,  0,    'scenario_alcaz_island_unused',                    1, 0),
+(951,  0,    'instance_nexus_tarecgosas_rest',                  1, 0),
+(971,  0,    'jade_forest_skyfire_gy',                          1, 0),
+(980,  0,    'arena_tolvir',                                    1, 0),
+(998,  0,    'battleground_deepwind_gorge',                     1, 0),
+(999,  0,    'scenario_theramore_horde',                        1, 0),
+(1001, 0,    'instance_scarlet_halls',                          0, 0),
+(1005, 0,    'scenario_brewing_storm',                          0, 0),
+(1007, 0,    'instance_scholomance_mop',                        0, 0),
+(1010, 0,    'battleground_ctf3_whatever',                      1, 0),
+(1014, 0,    'scenario_7.0_artifact_monk_peak_of_serenity',     1, 0),
+(1024, 0,    'scenario_greenstone_village',                     1, 0),
+(1029, 0,    'scenario_mogu_catacombs',                         0, 0),
+(1030, 0,    'scenario_crypt_of_forgotten_king',                0, 0),
+(1031, 0,    'scenario_ring_of_blood',                          1, 0),
+(1035, 0,    'scenario_kotmogu',                                1, 0),
+(1048, 0,    'scenario_oonga_ingoo',                            1, 0),
+(1049, 0,    'scenario_black_ox_temple',                        1, 0),
+(1050, 0,    'scenario_zanvess',                                1, 0),
+(1051, 0,    'scenario_brewmoon_festival',                      1, 0),
+(1060, 0,    'scenario_mining_prototype_for_6.0',               1, 0),
+(1061, 0,    'scenario_domination_point',                       1, 0),
+(1062, 0,    'scenario_lions_landing',                          1, 0),
+(1095, 0,    'scenario_knife_in_the_dark',                      1, 0),
+(1099, 0,    'scenario_battle_on_the_high_seas',                0, 0),
+(1102, 0,    'scenario_fort_horde',                             1, 0),
+(1103, 0,    'scenario_fort_alliance',                          1, 0),
+(1104, 0,    'scenario_patience',                               1, 0),
+(1105, 0,    'battleground_gold_rush',                          1, 0),
+(1106, 0,    'scenario_dalaran_for_garrosh_questline',          1, 0),
+(1107, 0,    'scenario_7.0_artifact_warlock_OH_and_7.2_mount',  1, 0),
+(1112, 0,    'scenario_warlock_black_temple_green_fire',        1, 0),
+(1120, 0,    'instance_isle_of_the_thunder_king_horde_boat',    1, 0),
+(1121, 0,    'instance_isle_of_the_thunder_king_alliance_boat', 1, 0),
+(1126, 1064, 'scenario_isle_of_the_thunder_progression',        1, 0),
+(1130, 0,    'scenario_shimmer',                                1, 0),
+(1131, 0,    'scenario_emberdeep',                              1, 0),
+(1134, 0,    'arena_shadopan',                                  1, 0),
+(1135, 1064, 'scenario_treasury_of_the_thunder_king',           1, 0),
+(1144, 0,    'scenario_dark_heart_of_pandaria',                 1, 0),
+(1148, 870,  'scenario_proving_grounds',                        0, 0),
+(1155, 0,    'scenario_5.5_defense',                            1, 0),
+(1157, 0,    'scenario_noodle_cart_in_halfhill',                1, 0),
+(1161, 870,  'scenario_celestial_tournament',                   0, 0),
+(1168, 1116, 'scenario_the_purge_of_grommar',                   1, 0),
+(1170, 0,    'scenario_boss_fight_arena',                       1, 0),
+(1171, 0,    'instance_small_battleground',                     1, 0),
+(1191, 1116, 'battleground_ashran',                             1, 0),
+(1200, 1116, 'scenario_frostfire_1_bonetown',                   1, 0),
+(1203, 1116, 'scenario_frostfire_2_finale',                     1, 0),
+(1207, 1116, 'scenario_shattrath_city',                         1, 0),
+(1268, 0,    'scenario_remains_of_xandros',                     1, 0),
+(1280, 0,    'battleground_hillsbrad_foothills',                1, 0),
+(1329, 1116, 'scenario_grommashar',                             1, 0),
+(1374, 1116, 'scenario_auchindoun_trial_of_fate',               1, 0),
+(1402, 1116, 'scenario_gorgrond_finale',                        1, 0),
+(1431, 0,    'battleground_the_coliseum',                       1, 0),
+(1451, 0,    'scenario_6.2_tanaan',                             1, 0),
+(1453, 0,    'scenario_scourge_of_northshire',                  1, 0),
+(1455, 0,    'scenario_edge_of_reality_darkfeather_mount',      1, 0),
+(1460, 0,    'scenario_broken_shore_7.0',                       1, 0),
+(1461, 0,    'scenario_aszuna',                                 1, 0),
+(1464, 1116, 'instance_tanaan_jungle_6.2',                      1, 0),
+(1468, 1220, 'scenario_vault_of_the_wardens',                   0, 0),
+(1469, 0,    'scenario_7.0_artifact_shaman_OH',                 1, 0),
+(1476, 0,    'scenario_stormheim_prequest_skyfire_ship',        0, 0),
+(1478, 1220, 'scenario_valsharah_temple_of_elune',              1, 0),
+(1479, 1220, 'scenario_7.0_warrior_artifact_OH',                1, 0),
+(1480, 0,    'scenario_7.0_deathknight_artifact_icecrown',      1, 0),
+(1481, 0,    'scenario_7.0_dh_artifact_mardum',                 1, 0),
+(1489, 0,    'scenario_7.0_artifact',                           1, 0),
+(1494, 1220, 'scenario_7.0_artifact_violet_hold',               1, 0),
+(1495, 1220, 'scenario_7.0_artifact_shields_rest',              1, 0),
+(1500, 1220, 'scenario_7.0_artifact_brokenshore',               1, 0),
+(1502, 1220, 'scenario_7.0_artifact_shadowgore',                1, 0),
+(1503, 1220, 'scenario_7.0_artifact_ench_shaman',               1, 0),
+(1504, 0,    'arena_blackrook_hold',                            1, 0),
+(1505, 0,    'arena_nagrand_legion',                            1, 0),
+(1511, 1220, 'scenario_7.0_artifact_fury_surv',                 1, 0),
+(1512, 1220, 'scenario_7.0_artifact_priest_OH',                 1, 0),
+(1513, 571,  'scenario_7.0_artifact_oculus',                    1, 0),
+(1514, 0,    'scenario_7.0_artifact_monk_OH',                   1, 0),
+(1515, 1220, 'instance_azshara_highmountains_flashback',        1, 0),
+(1519, 0,    'scenario_7.0_artifact_dh_OH',                     1, 0),
+(1522, 1220, 'scenario_dreadscar_rift_warlock',                 1, 0),
+(1523, 0,    'scenario_mardum_start',                           1, 0),
+(1526, 1220, 'scenario_7.0_artifact_elemental_shaman',          1, 0),
+(1528, 0,    'scenario_7.0_artifact_skywall',                   1, 0),
+(1529, 0,    'scenario_karazhan_dungeon',                       0, 0),
+(1530, 1220, 'instance_nighthold',                              1, 0),
+(1536, 571,  'scenario_7.0_artifact_ursoks_lair',               0, 0),
+(1537, 0,    'scenario_boost_expirience_old',                   0, 0),
+(1539, 0,    'scenario_7.0_artifact_tirisfal_glades',           1, 0),
+(1540, 0,    'scenario_7.0_artifact_dreamway',                  1, 0),
+(1541, 0,    'scenario_7.0_artifact_mistweaver',                1, 0),
+(1545, 1220, 'scenario_7.0_artifact_combat_rogue',              1, 0),
+(1552, 0,    'arena_ashamane',                                  1, 0),
+(1553, 0,    'scenario_artifact_gloaming_reef',                 1, 0),
+(1557, 0,    'scenario_legion_boost_horde',                     0, 0),
+(1572, 0,    'scenario_7.0_artifact_shaman_intro',              0, 0),
+(1579, 0,    'scenario_7.0_artifact_ulduar',                    0, 0),
+(1580, 0,    'scenario_artifact_maelstrom',                     0, 0),
+(1583, 0,    'scenario_7.0_artifact_nexus_vault',               0, 0),
+(1599, 0,    'scenario_7.0_artifact_resto_druid',               0, 0),
+(1600, 0,    'scenario_7.0_artifact_resto_shaman',              0, 0),
+(1602, 0,    'scenario_7.0_artifact_vortex_pinnacle',           0, 0),
+(1604, 0,    'scenario_7.0_artifact_niskara',                   0, 0),
+(1605, 0,    'scenario_7.0_artifact_firelands',                 1, 0),
+(1607, 0,    'scenario_7.0_artifact_subtlety',                  0, 0),
+(1608, 0,    'scenario_7.0_artifact_malornes_nightmare',        1, 0),
+(1609, 0,    'scenario_7.0_artifact_beastmaster',               0, 0),
+(1611, 0,    'scenario_7.0_artifact_lights_hope',               0, 0),
+(1612, 0,    'scenario_7.0_artifact_ruins_of_falanaar',         0, 0),
+(1616, 0,    'scenario_aoe_challenge_and_frost_artifact',       0, 0),
+(1617, 0,    'scenario_7.0_artifact_rescue_coltira',            1, 0),
+(1618, 0,    'scenario_7.0_artifact_dk_scarlet_monastery',      0, 0),
+(1620, 0,    'scenario_7.0_artifact_assassination_rogue',       0, 0),
+(1621, 0,    'scenario_black_temple',                           0, 0),
+(1623, 0,    'scenario_mage_campaign_oculus',                   1, 0),
+(1624, 0,    'scenario_illidans_origins',                       0, 0),
+(1625, 0,    'scenario_7.0_artifact_brewmaster',                0, 0),
+(1626, 0,    'scenario_the_collapse',                           0, 0),
+(1629, 0,    'scenario_defense_of_netherlight_temple',          0, 0),
+(1630, 0,    'scenario_7.0_artifact_tolbarad',                  0, 0),
+(1632, 0,    'scenario_7.0_artifact_arcway',                    0, 0),
+(1646, 0,    'scenario_illidans_origins_2',                     0, 0),
+(1648, 1220, 'instance_trial_of_valor',                         1, 0),
+(1651, 0,    'instance_karazhan_legion',                        0, 0),
+(1653, 0,    'scenario_illidans_origins_3',                     0, 0),
+(1662, 1220, 'scenario_suramar_campaign_finale',                1, 0),
+(1666, 1220, 'scenario_7.2_broken_shore_intro',                 1, 0),
+(1672, 0,    'arena_blades_edge_new',                           1, 0),
+(1673, 0,    'scenario_single_challenge',                       0, 0),
+(1677, 1220, 'instance_cathedral_of_eternal_night',             0, 0),
+(1681, 0,    'battleground_arathi_basin_winter_brawl',          1, 0),
+(1683, 0,    'scenario_challenge_stealth',                      0, 0),
+(1684, 0,    'scenario_challenge_twins',                        0, 0),
+(1687, 1,    'scenario_wailing_caverns_pet_dungeon',            0, 0),
+(1692, 0,    'scenario_7.2_dk_campaign_pit_of_saron',           1, 0),
+(1693, 0,    'scenario_7.2_monk_brewmaster',                    1, 0),
+(1695, 0,    'scenario_7.2_brokenshore_legion_commandship',     0, 0),
+(1698, 0,    'scenario_challenge_tank',                         0, 0),
+(1702, 0,    'scenario_challenge_dualboss',                     0, 0),
+(1703, 0,    'scenario_challenge_godqueen',                     0, 0),
+(1704, 1220, 'scenario_invasion_valsharah',                     1, 0),
+(1705, 1220, 'scenario_invasion_azsuna',                        1, 0),
+(1706, 1220, 'scenario_invasion_highmountain',                  1, 0),
+(1707, 1220, 'scenario_invasion_stormheim',                     1, 0),
+(1708, 0,    'scenario_7.2_paladin_class_mount',                1, 0),
+(1710, 0,    'scenario_challenge_healer',                       0, 0),
+(1714, 0,    'scenario_7.2_warrior_class_mount',                1, 0),
+(1715, 0,    'battleground_blackrock_mountain_brawl',           1, 0),
+(1719, 0,    'scenario_7.2_warlock_class_mount',                1, 0),
+(1728, 0,    'scenario_7.2_hunter_class_mount',                 1, 0),
+(1729, 0,    'scenario_7.2_mage_class_mount',                   1, 0),
+(1730, 0,    'scenario_7.2_druid_class_mount1',                 1, 0),
+(1731, 0,    'scenario_7.2_druid_class_mount2',                 1, 0),
+(1732, 0,    'scenario_7.2_dh_class_mount',                     1, 0),
+(1734, 0,    'scenario_7.2_shaman_class_mount',                 1, 0),
+(1736, 0,    'scenario_7.2_dk_class_mount',                     1, 0),
+(1737, 0,    'scenario_7.2_akazamaks_hat',                      0, 0),
+(1738, 0,    'scenario_7.2_dk_class_mount2',                    1, 0); 
+
+UPDATE `instance_template` SET `parent`=571  WHERE `map` IN (631, 632, 658, 668, 724);
+UPDATE `instance_template` SET `parent`=1    WHERE `map` IN (644, 657, 720, 754, 755, 938, 939, 940, 967);
+UPDATE `instance_template` SET `parent`=646  WHERE `map`=725;
+UPDATE `instance_template` SET `parent`=732  WHERE `map`=757;
+UPDATE `instance_template` SET `parent`=870  WHERE `map` IN (1009);
+UPDATE `instance_template` SET `parent`=1116 WHERE `map` IN (1182, 1195, 1205, 1208, 1209, 1277, 1279);
+UPDATE `instance_template` SET `parent`=1220 WHERE `map` IN (1456, 1458, 1462, 1477, 1493, 1516, 1544);
+
+UPDATE `instance_template` SET `allowMount`=1 WHERE `map` IN (996, 1000, 1004, 1136, 1277, 1458, 1466, 1477, 1492, 1498, 1501, 1520, 1571, 1676);
+
+UPDATE `instance_template` SET `script`='instance_terrace_of_the_endless_spring'       WHERE `map`=996;
+UPDATE `instance_template` SET `script`='scenario_theramore_alliance'                  WHERE `map`=1000;
+UPDATE `instance_template` SET `script`='instance_throne_of_thunder'                   WHERE `map`=1098;
+UPDATE `instance_template` SET `script`='instance_siege_of_orgrimmar'                  WHERE `map`=1136;
+UPDATE `instance_template` SET `script`='instance_horde_garrison_tier_1'               WHERE `map`=1152;
+UPDATE `instance_template` SET `script`='instance_horde_garrison_tier_3'               WHERE `map`=1153;
+UPDATE `instance_template` SET `script`='instance_alliance_garrison_tier_1'            WHERE `map`=1158;
+UPDATE `instance_template` SET `script`='instance_alliance_garrison_tier_3'            WHERE `map`=1159;
+UPDATE `instance_template` SET `script`='instance_bloodmaul_slag_mines'                WHERE `map`=1175;
+UPDATE `instance_template` SET `script`='scenario_defense_of_karabor'                  WHERE `map`=1277;
+UPDATE `instance_template` SET `script`='instance_horde_garrison_tier_2'               WHERE `map`=1330;
+UPDATE `instance_template` SET `script`='instance_alliance_garrison_tier_2'            WHERE `map`=1331;
+UPDATE `instance_template` SET `script`='instance_upper_blackrock_spire_wod'           WHERE `map`=1358;
+UPDATE `instance_template` SET `script`='instance_hellfire_citadel'                    WHERE `map`=1448;
+UPDATE `instance_template` SET `script`='scenario_stormheim_prequest'                  WHERE `map`=1475;
+UPDATE `instance_template` SET `script`='instance_auchindoun'                          WHERE `map`=1182;
+UPDATE `instance_template` SET `script`='instance_iron_docks'                          WHERE `map`=1195;
+UPDATE `instance_template` SET `script`='instance_blackrock_foundry'                   WHERE `map`=1205;
+UPDATE `instance_template` SET `script`='instance_grim_depot'                          WHERE `map`=1208;
+UPDATE `instance_template` SET `script`='instance_skyreach'                            WHERE `map`=1209;
+UPDATE `instance_template` SET `script`='instance_neltharions_lair'                    WHERE `map`=1458;
+UPDATE `instance_template` SET `script`='scenario_twisting_nether_masters_promontory'  WHERE `map`=1462;
+UPDATE `instance_template` SET `script`='instance_vault_of_the_wardens'                WHERE `map`=1493;
+UPDATE `instance_template` SET `script`='instance_arcway'                              WHERE `map`=1516;
+UPDATE `instance_template` SET `script`='instance_violet_hold_legion'                  WHERE `map`=1544;
+UPDATE `instance_template` SET `script`='scenario_7.0_artifact_legion_ship'            WHERE `map`=1498;
+UPDATE `instance_template` SET `script`='scenario_7.0_artifact_blackrook_hold'         WHERE `map`=1501;
+UPDATE `instance_template` SET `script`='scenario_7.0_artifact_karazhan_catacombs'     WHERE `map`=1533;
+UPDATE `instance_template` SET `script`='scenario_legion_boost_alliance'               WHERE `map`=1554;
