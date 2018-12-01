972549afb70200d3accb6c8461f3bb576e9a2d47
Kito <kito@vortexirc.com>
2018-12-01 18:57:09 +0100

    Fix RBAC

--- sql/ashamane/world/2018_12_01_01_spells.sql
+++ sql/ashamane/world/2018_12_01_01_spells.sql
@@ -34,4 +34,4 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 
 DELETE FROM `command` WHERE `name`='reload spell_script_names';
 INSERT INTO `command` (`name`, `permission`, `help`) VALUES 
-('reload spell_script_names', 2008, 'Syntax: .reload spell_script_names');
+('reload spell_script_names', 2009, 'Syntax: .reload spell_script_names');
