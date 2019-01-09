7cb08b1113ccc3854227e2e2f37ab9faf964c111
Ulduar <Ulduar@users.noreply.github.com>
2019-01-09 07:24:06 +0500

    Kiljaeden Ship Teleport.
    Add game_tele.

---
+++ sql/ashamane/world/2019_01_09_01_world_game_tele.sql
@@ -0,0 +1,3 @@
+DELETE FROM `game_tele` WHERE `id` = 10000;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES
+(10000, 4500.37, -1098.37, 335.967, 4.66883, 1676,'LegionTombofSargerasKiljaedenShip');
