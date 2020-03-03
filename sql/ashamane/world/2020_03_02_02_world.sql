e996505a0f5c6c8ba51f380df582eafe83a92e43
cooler-SAI <cooler19860212@yahoo.com>
2020-03-03 23:19:14 +0300

    DB/SAI: typo
    (cherry picked from commit 1b552290d9665292c72b60369b88f5cdb656829a)

--- sql/ashamane/world/2020_03_02_02_world.sql
+++ sql/ashamane/world/2020_03_02_02_world.sql
@@ -107,37 +107,9 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,0,0,3000,3500,11,9532,64,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Lightning Bolt'),
 (@ENTRY,0,1,0,2,0,100,1,0,15,0,0,25,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Flee at 15% HP'),
-(@ENTRY,0,2,0,1,0,100,0,500,1000,600000,600000,11,12550,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast xxxxx on Spawn'),
-(@ENTRY,0,3,0,16,0,100,0,12550,1,15000,30000,11,12550,1,0,0,0,0,1,0,0,0,0,0,0,0,'Cast xxxxx on Repeat'),
+(@ENTRY,0,2,0,1,0,100,0,500,1000,600000,600000,11,12550,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Lightning Shield on Spawn'),
+(@ENTRY,0,3,0,16,0,100,0,12550,1,15000,30000,11,12550,1,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Lightning Shield on Repeat'),
 (@ENTRY,0,4,5,8,0,100,0,73108,0,5000,5000,33,38887,0,0,0,0,0,7,0,0,0,0,0,0,0,"Quest Kredit Kill Monster on Spell Hit"),
 (@ENTRY,0,5,0,61,0,100,0,0,0,0,0,33,38923,0,0,0,0,0,7,0,0,0,0,0,0,0,'Quest Kredit Kill Monster on Spell Hit');
 DELETE FROM `gameobject` WHERE `guid` = '103438'; 
 DELETE FROM `creature_loot_template` WHERE `Item` = '2875'; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
