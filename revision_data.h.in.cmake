5f64372f612949c5dea43667ad9babcdc7739d2f
Greaver <t4cpirates@hotmail.com>
2021-09-25 02:13:08 +0300

    Update DB Version to ADB735.10

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -9,8 +9,8 @@
  #define _SOURCE_DIRECTORY          R"(@CMAKE_SOURCE_DIR@)"
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
- #define _FULL_DATABASE             "ADB_world_735.00.sql"
- #define _HOTFIXES_DATABASE         "ADB_hotfixes_735.00.sql"
+ #define _FULL_DATABASE             "ADB_world_735.10.sql"
+ #define _HOTFIXES_DATABASE         "ADB_hotfixes_735.10.sql"
  #define VER_COMPANYNAME_STR        "Ashamane Developers"
  #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2018 AshamaneCore"
  #define VER_FILEVERSION            0,0,0
